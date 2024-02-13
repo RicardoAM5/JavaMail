40951b80bc116a653a043a8405588c50eea5678c
Ricardo Alonso Medina <ricardoalonsomedina99@gmail.com>
2024-02-13 11:47:50 -0600

    Initial

---
+++ springJavaMail/springJavaMail/src/main/java/com/ricardoam5/springJavaMail/SpringJavaMailApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoam5.springJavaMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaMailApplication.class, args);
+	}
+
+}
---
+++ springJavaMail/springJavaMail/src/test/java/com/ricardoam5/springJavaMail/SpringJavaMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoam5.springJavaMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
